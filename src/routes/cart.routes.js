a3acddb2a7ec333c27928c5ea7f5a638bcc688f7
Gabiel Andriossi <gaboandriossi@gmail.com>
2024-05-17 23:54:22 -0300

    auth fixed

--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { saveCart, getCarts, getCartDetails, addProduct, deleteProduct, empty, deleteCart, updateQuantity, findById} from "../controllers/cart.controller.js";
 import authUser from "../middlewares/authUser.js";
+import authUserorAdmin from "../middlewares/authUserorAdmin.js";
 
 const cartsRouter = express.Router();
 
@@ -8,11 +9,11 @@ cartsRouter.post("/", saveCart);
 cartsRouter.get("/:cid", getCartDetails);
 cartsRouter.get("/:cartId", findById);
 cartsRouter.get("/", getCarts);
-cartsRouter.post("/:cid/product", addProduct);
-cartsRouter.delete("/:cid/product/:productEntryId", deleteProduct);
-cartsRouter.delete("/:cid/empty", empty);
+cartsRouter.post("/:cid/product", authUserorAdmin, addProduct);
+cartsRouter.delete("/:cid/product/:productEntryId", authUserorAdmin, deleteProduct);
+cartsRouter.delete("/:cid/empty", authUserorAdmin, empty);
 cartsRouter.delete("/:cid", deleteCart)
-cartsRouter.put("/:cid/product/:productId/quantity", updateQuantity);
+cartsRouter.put("/:cid/product/:productId/quantity", authUserorAdmin, updateQuantity);
 
 export default cartsRouter;
 
